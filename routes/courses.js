880f73e4238004a2c721357d80f273673ad2ad6f
vanik <egoyanvanik@gmail.com>
2026-03-16 17:39:35 +0300

    add remove course

--- routes/courses.js
+++ routes/courses.js
@@ -38,4 +38,15 @@ router.post('/edit', async (req, res) => {
   res.redirect('/courses')
 })
 
+router.post('/remove', async (req, res) => {
+  try {
+    await Course.deleteOne({
+      _id: req.body.id,
+    })
+    res.redirect('/courses')
+  } catch (e) {
+    console.log(e)
+  }
+})
+
 module.exports = router
